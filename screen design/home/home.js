beefe996bf7ad81b44a44ae0c6548199c96f12bb
tranminhtoan149 <tranminhtoan.149@gmail.com>
2022-03-24 17:19:26 +0700

    fix css for home

--- screen design/home/home.js
+++ screen design/home/home.js
@@ -2,12 +2,14 @@ $(".on-off-button").click(function () {
     if ($(this).hasClass("check-on-off")) {
         $(this).removeClass("check-on-off");
         $(this).children().css("background-color", "white");
+        $(this).parent().css("background-color", "rgb(255, 93, 93)");
         $(this).children().animate({
             left: "2px",
         });
     } else {
         $(this).addClass("check-on-off");
         $(this).children().css("background-color", "black");
+        $(this).parent().css("background-color", "rgb(131, 248, 170)");
         $(this).children().animate({
             left: "26px",
         });
