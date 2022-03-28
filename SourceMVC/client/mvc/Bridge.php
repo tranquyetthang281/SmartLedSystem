0682623c9ee8e488346e3e7ab6ecce4211ac2c82
TranQuyetThang <tranquyetthang281@gmail.com>
2022-03-28 22:37:38 +0700

    add AdafruitIo class

--- SourceMVC/client/mvc/Bridge.php
+++ SourceMVC/client/mvc/Bridge.php
@@ -6,6 +6,8 @@ require_once "./mvc/core/Controller.php";
 
 require_once "./mvc/core/Database.php";
 
+require_once "./mvc/core/AdafruitIO.php";
+
 require_once "./mvc/core/Session.php";
 
 require_once "./mvc/core/Role.php";
